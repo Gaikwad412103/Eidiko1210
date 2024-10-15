dd5b9edb5f7676315127b7d84a842f5ff8b9c8ca
Gaikwad412103 <avinashgaikwad.eidiko@gmail.com>
2024-10-15 03:29:56 -0700

    "Role Based Authentication Added"

--- JavaWork/spring-security/src/main/java/com/eidiko/spring_security/controller/HomeController.java
+++ JavaWork/spring-security/src/main/java/com/eidiko/spring_security/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.eidiko.spring_security.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,12 @@ public class HomeController {
     public ResponseEntity<String> home(){
         return ResponseEntity.ok("Welcome To The Home Page!!!");
     }
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     public ResponseEntity<String> users(){
         return ResponseEntity.ok("Hello User!!");
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public ResponseEntity<String> admin(){
         return ResponseEntity.ok("Hello Admin!!");
