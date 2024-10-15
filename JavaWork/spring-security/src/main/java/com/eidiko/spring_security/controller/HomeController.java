748433ae369a03fd7dff682dc48c2a316429d1e5
Gaikwad412103 <avinashgaikwad.eidiko@gmail.com>
2024-10-15 02:28:50 -0700

    "Default Security Added (Initial Commit)"

---
+++ JavaWork/spring-security/src/main/java/com/eidiko/spring_security/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.eidiko.spring_security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Welcome To The Home Page!!!");
+    }
+}
