84b30a56c7e4080b866f12987e6bd5641b470c43
Gaikwad412103 <avinashgaikwad.eidiko@gmail.com>
2024-10-18 06:34:35 -0700

    "Initial Commit"

---
+++ JavaWork/spring-jwt-example/src/main/java/com/security/springboot3_security_by_jwt/repositories/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.security.springboot3_security_by_jwt.repositories;
+
+import com.security.springboot3_security_by_jwt.entities.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
---
+++ JavaWork/spring-jwt-example/src/test/java/com/security/springboot3_security_by_jwt/Springboot3SecurityByJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springboot3_security_by_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3SecurityByJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
